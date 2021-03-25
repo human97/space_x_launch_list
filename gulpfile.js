b8116da4c05085aac21a193fb85b92c8a32fb28a
human97 <aleksey.n.popov@gmail.com>
2021-03-25 10:37:57 +0400

    start project

---
+++ gulpfile.js
@@ -0,0 +1,167 @@
+"use strict";
+
+const {
+    src,
+    dest
+} = require("gulp");
+const gulp = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const cssbeautify = require("gulp-cssbeautify");
+const removeComments = require('gulp-strip-css-comments');
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const cssnano = require("gulp-cssnano");
+const babel = require("gulp-babel");
+const rigger = require("gulp-rigger");
+const uglify = require("gulp-uglify");
+const terser = require('gulp-terser');
+const plumber = require("gulp-plumber");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+const panini = require("panini");
+const browsersync = require("browser-sync").create();
+
+
+/* Paths */
+const path = {
+    build: {
+        html: "dist/",
+        js: "dist/assets/js/",
+        css: "dist/assets/css/",
+        images: "dist/assets/img/",
+        fonts: "dist/assets/fonts/"
+    },
+    src: {
+        html: "src/*.html",
+        js: "src/assets/js/*.js",
+        css: "src/assets/sass/style.scss",
+        images: "src/assets/img/**/*.{jpg,png,svg,gif,ico,webmanifest,xml,webp,avif}",
+        fonts: "src/assets/fonts/**/*"
+    },
+    watch: {
+        html: "src/**/*.html",
+        js: "src/assets/js/**/*.js",
+        css: "src/assets/sass/**/*.scss",
+        images: "src/assets/img/**/*.{jpg,png,svg,gif,ico,webmanifest,xml,webp,avif}",
+        fonts: "dist/assets/fonts/**/*"
+    },
+    clean: "./dist"
+}
+
+
+/* Tasks */
+
+function browserSync(done) {
+    browsersync.init({
+        server: {
+            baseDir: "./dist/"
+        },
+        port: 3000
+    });
+}
+
+function browserSyncReload(done) {
+    browsersync.reload();
+}
+
+const html = () => {
+    panini.refresh();
+    return src(path.src.html, {
+            base: "src/"
+        })
+        .pipe(plumber())
+        .pipe(panini({
+            root: 'src/',
+            layouts: 'src/tpl/layouts/',
+            partials: 'src/tpl/partials/',
+            helpers: 'src/tpl/helpers/',
+            data: 'src/tpl/data/'
+        }))
+        .pipe(dest(path.build.html))
+        .pipe(browsersync.stream());
+}
+
+const css = () => {
+    return src(path.src.css, {
+            base: "src/assets/sass/"
+        })
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            Browserslist: ['last 10 versions'],
+            cascade: true
+        }))
+        .pipe(cssbeautify())
+        .pipe(dest(path.build.css))
+        .pipe(cssnano({
+            zindex: false,
+            discardComments: {
+                removeAll: true
+            }
+        }))
+        .pipe(removeComments())
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".css"
+        }))
+        .pipe(dest(path.build.css))
+        .pipe(browsersync.stream());
+}
+
+const js = () => {
+    return src(path.src.js, {
+            base: './src/assets/js/'
+        })
+        .pipe(plumber())
+        .pipe(rigger())
+        .pipe(babel({
+            presets: ['@babel/preset-env']
+        }))
+        .pipe(gulp.dest(path.build.js))
+        .pipe(terser())
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".js"
+        }))
+
+        .pipe(dest(path.build.js))
+        .pipe(browsersync.stream());
+}
+
+const images = () => {
+    return src(path.src.images)
+        .pipe(imagemin())
+        .pipe(dest(path.build.images));
+}
+
+const fonts = () => {
+    return src(path.src.fonts)
+        .pipe(dest(path.build.fonts));
+}
+
+const clean = () => {
+    return del(path.clean);
+}
+
+const watchFiles = () => {
+    gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.css], css);
+    gulp.watch([path.watch.js], js);
+    gulp.watch([path.watch.images], images);
+    gulp.watch([path.watch.fonts], fonts);
+}
+
+const build = gulp.series(clean, gulp.parallel(html, css, js, images, fonts));
+const watch = gulp.parallel(build, watchFiles, browserSync);
+
+
+/* Exports Tasks */
+exports.html = html;
+exports.css = css;
+exports.js = js;
+exports.images = images;
+exports.fonts = fonts;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
