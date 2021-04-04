e8217a6d5e37726bf27e64d50a7a427a78bbcd74
human97 <aleksey.n.popov@gmail.com>
2021-04-04 21:42:05 +0400

    fix picture

--- src/assets/js/components/add_news.js
+++ src/assets/js/components/add_news.js
@@ -35,9 +35,9 @@ for (let key of news) {
 
     <picture class="block_img--mission_img">
 
-      <source class="mission_img" srcset="assets/img/missions/mission_img_${key.Mission}.avif" media='(min-width: 1220px)' type="image/avif">
+      <source class="mission_img" srcset="assets/img/missions/mission_img_${key.Mission}.avif" type="image/avif">
 
-      <source class="mission_img" srcset="assets/img/missions/mission_img_${key.Mission}.webp" media='(min-width: 1220px)' type="image/webp">
+      <source class="mission_img" srcset="assets/img/missions/mission_img_${key.Mission}.webp" type="image/webp">
 
       <img class="mission_img" src="assets/img/missions/mission_img_${key.Mission}.png" srcset="assets/img/missions/mission_img_${key.Mission}x2.png 2x" alt="image">
 
